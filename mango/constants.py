2c4cf47d21cecf237587529e95e3b4c5f5c59292
genedan <genedan@gmail.com>
2022-06-24 20:19:09 -0500

    Add constants for unit test precision and project path.

--- mango/constants.py
+++ mango/constants.py
@@ -1,1 +1,9 @@
+import os
+
 BUILD_VERSION = '0.0.0'
+
+# Number of decimal places for floating-point unit test assertions
+TESTING_PRECISION = 8
+
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
