8c249df50b723fe99f5e34c59537eb64bad4da96
genedan <genedan@gmail.com>
2022-06-28 22:27:27 -0500

    Initialize unit test q19_2005.py.

---
+++ tests/game_theory/q19_2005.py
@@ -0,0 +1,24 @@
+"""
+Taken from CAS 2005 exam, question 19
+"""
+import pandas as pd
+
+events = [1, 2, 3]
+
+probs = [.05, .005, .0005]
+
+data_x = pd.DataFrame.from_dict(
+    data={
+        'event': events,
+        'probability': probs,
+        'loss': [250, 1000, 6000]
+    }
+)
+
+data_y = pd.DataFrame.from_dict(
+    data={
+        'event': events,
+        'probability': probs,
+        'loss': [60, 300, 1000]
+    }
+)
