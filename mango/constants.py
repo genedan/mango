6f325fffe66bac5e61252d8c8f975f2b68c9fb88
Gene Dan <genedan@gmail.com>
2022-07-05 22:46:03 -0500

    add annotations

--- mango/constants.py
+++ mango/constants.py
@@ -5,5 +5,5 @@ BUILD_VERSION = '0.0.0'
 # Number of decimal places for floating-point unit test assertions
 TESTING_PRECISION = 8
 
-
+# The path name of the repo
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
